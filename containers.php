34096c469dbd91a86948c6267eb47fe72f753770
Stu Robson <stu@alwaystwisted.com>
2015-09-14 14:12:32 +0100

    splits out blocks and containers as separate loaded things

--- containers.php
+++ containers.php
@@ -49,69 +49,25 @@
     <div id="nav" class="sg-sidebar" role="navigation">
       <div class="sg-nav-group sg-nav-group--about">
         <h2 class="sg-nav-h2 sg-subnav-title"><a href="index.php">About</a></h2>
-        <ul class="sg-navlist">
-          <li>
-            <a href="#sg-about">Getting Started</a>
-          </li>
-          <li>
-            <a href="#sg-colors">Colors</a>
-          </li>
-          <li>
-            <a href="#sg-fontStacks">Fonts</a>
-          </li>
-        </ul>
       </div>
 
       <div class="sg-nav-group">
         <h2 class="sg-nav-h2 sg-subnav-title"><a href="blocks.php">Blocks</a></h2>
       </div>
       <div class="sg-nav-group">
-        <h2 class="sg-nav-h2 sg-subnav-title"><a href="containers.php">Containers</a></h2>
+        <h2 class="sg-nav-h2 sg-subnav-title">Containers <span class="sg-toggle">more</span></h2>
+        <ul class="sg-navlist">
+          <?php listMarkupAsListItems('containers'); ?>
+        </ul>
       </div>
     </div><!--/.sg-sidebar-->
 
     <div id="main" class="sg-main" role="main">
       <div class="sg-container">
-        <div class="sg-info">
-          <div class="sg-about sg-section">
-            <h2 id="sg-about" class="sg-h2">Getting Started</h2>
-            <p>A living style guide is a great tool to promote visual consistency, unify UX designers and front-end developers, as well as speed up development times. Add some documentation here on how to get started with your new style guide and start customizing this boilerplate to your liking.</p>
-            <p>If you are looking for resources on style guides, check out <a href="http://styleguides.io">styleguides.io</a>. There are a ton of great articles, books, podcasts, talks, and other style guide tools!</p>
-          </div><!--/.sg-about-->
-
-          <!-- Manually add your UI colors here. -->
-          <div class="sg-colors sg-section" id="color-grid">
-            <h2 id="sg-colors" class="sg-h2">Colors</h2>
-            <div class="sg-color-grid">
-
-            </div><!--/.sg-color-grid-->
-          </div><!--/.sg-colors-->
-
-          <!-- Manually add your fonts here. -->
-          <div class="sg-font-stacks sg-section">
-            <h2 id="sg-fontStacks" class="sg-h2">Font Stacks</h2>
-            <dl class="sg-font-list">
-              <dt>Primary Font:</dt>
-              <dd style='font-family: "HelveticaNeue", Helvetica, Arial, sans-serif;'>"HelveticaNeue", Helvetica, Arial, sans-serif;</dd>
-
-              <dt>Primary Font Italic:</dt>
-              <dd style='font-family: "HelveticaNeue", Helvetica, Arial, sans-serif; font-style: italic;'>"HelveticaNeue", Helvetica, Arial, sans-serif;</dd>
-
-              <dt>Primary Font Bold:</dt>
-              <dd style='font-family: "HelveticaNeue", Helvetica, Arial, sans-serif; font-weight: 800;'>"HelveticaNeue", Helvetica, Arial, sans-serif;</dd>
-
-              <dt>Secondary Font:</dt>
-              <dd style='font-family: Georgia, Times, "Times New Roman", serif;'>Georgia, Times, "Times New Roman", serif;</dd>
-
-              <dt>Secondary Font Italic:</dt>
-              <dd style='font-family: Georgia, Times, "Times New Roman", serif; font-style: italic;'>Georgia, Times, "Times New Roman", serif;</dd>
-
-              <dt>Secondary Font Bold:</dt>
-              <dd style='font-family: Georgia, Times, "Times New Roman", serif; font-weight: 800;'>Georgia, Times, "Times New Roman", serif;</dd>
-            </dl>
-            <div class="sg-markup-controls"><a class="sg-btn--top" href="#top">Back to Top</a></div>
-          </div><!--/.sg-font-stacks-->
-        </div><!--/.sg-info-->
+        <div class="sg-base-styles">
+          <h1 class="sg-h1">Containers<small> - Page Containers</small></h1>
+          <?php showMarkup('containers'); ?>
+        </div>
 
       </div><!--/.sg-container-->
     </div><!--/.sg-main-->
