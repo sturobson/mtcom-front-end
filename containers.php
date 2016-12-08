9926502fa1cfcbb0cf79f8a6fb46b1a94dbebf76
Stu Robson <stu@alwaystwisted.com>
2016-12-08 13:10:04 +0000

    adds all the new patterns

--- containers.php
+++ containers.php
@@ -130,5 +130,34 @@
       return "blocks.php" + v;
     });
     </script>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+<script>
+$('#js-nav-drawer').click(function(){
+  $('html').toggleClass('has-active-menu');
+  $('.mt-b-nav__items--drawer').toggleClass('is-active');
+  $(this).toggleClass('active-button');
+  if ($(this).text() == "Menu")
+    $(this).text("Close Menu")
+  else
+    $(this).text("Menu");
+});
+$(document).keyup(function(e) {
+  if (e.keyCode == 27) { // escape key maps to keycode `27`
+    $('html').removeClass('has-active-menu');
+    $('.mt-b-nav__items--drawer').removeClass('is-active');
+    $('#js-nav-drawer').text("Menu");
+  }
+});
+$('.mt-b-nav__item--search span').click(function(){
+  if ($(this).text() == 'Search')
+  $(this).text('Close Search')
+  else
+  $(this).text('Search');
+  $('.mt-b-search').toggleClass('search-open');
+  return false;
+});
+</script>
   </body>
   </html>
